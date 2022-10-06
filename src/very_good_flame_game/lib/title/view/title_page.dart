9bbe0129b300f204cf9a325511ad23266d4fc3ee
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-10-06 13:58:37 +0200

    feat: title screen

---
+++ src/very_good_flame_game/lib/title/view/title_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:very_good_flame_game/l10n/l10n.dart';
+
+class TitlePage extends StatelessWidget {
+  const TitlePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(l10n.titleAppBarTitle),
+      ),
+      body: const SafeArea(child: TitleView()),
+    );
+  }
+}
+
+class TitleView extends StatelessWidget {
+  const TitleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Center(
+      child: SizedBox(
+        width: 250,
+        height: 64,
+        child: ElevatedButton(
+          onPressed: () {
+            // TODO(wolfen): Navigate to game page
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (context) => const Scaffold(),
+              ),
+            );
+          },
+          child: Center(child: Text(l10n.titleButtonStart)),
+        ),
+      ),
+    );
+  }
+}
