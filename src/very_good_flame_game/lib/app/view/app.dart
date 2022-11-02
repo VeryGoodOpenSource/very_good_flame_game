b18718af8bab2648c69e6d38e244d8b8ebbb5c1e
Felix Angelov <felangelov@gmail.com>
2022-11-02 10:04:29 -0500

    remove copyright headers

--- src/very_good_flame_game/lib/app/view/app.dart
+++ src/very_good_flame_game/lib/app/view/app.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flame/cache.dart';
 import 'package:flutter/material.dart';
