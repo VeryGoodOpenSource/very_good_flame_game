04e92be2d9bf17f116fcb6161b5de4f6884b9b4d
Alejandro Santiago <dev@alestiago.com>
2023-04-25 17:32:23 +0100

    Update brick/__brick__/{{project_name.snakeCase()}}/test/helpers/mocks.dart

--- brick/__brick__/{{project_name.snakeCase()}}/test/helpers/mocks.dart
+++ brick/__brick__/{{project_name.snakeCase()}}/test/helpers/mocks.dart
@@ -3,7 +3,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:{{project_name.snakeCase()}}/loading/loading.dart';
 
-class MockPreloadCubit extends MockCubit<PreloadState>
-    implements PreloadCubit {}
+class MockPreloadCubit extends MockCubit<PreloadState> implements PreloadCubit {}
 
 class MockAudioCache extends Mock implements AudioCache {}
